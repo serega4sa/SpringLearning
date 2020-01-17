d4c26b499c8b4333d533fe4b8239ec2404fd84dc
Serhii Chmykhun <serega4sa@gmail.com>
2020-01-17 12:57:28 +0200

    [SL] - DI through setters (xml config)

--- src/main/java/com/chmykhun/springlearning/MusicPlayer.java
+++ src/main/java/com/chmykhun/springlearning/MusicPlayer.java
@@ -3,13 +3,42 @@ package com.chmykhun.springlearning;
 public class MusicPlayer {
 
     private Music music;
+    private String name;
+    private int volume;
+
+    public MusicPlayer() {
+    }
 
     // IoC
     public MusicPlayer(Music music) {
         this.music = music;
     }
 
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        System.out.println("Playing: " + music.getSong() + " (singer: " + name + ", volume: " + volume + ")");
     }
 }
